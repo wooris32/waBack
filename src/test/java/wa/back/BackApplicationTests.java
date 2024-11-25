76bde6d2ab766ff0cef38c74e93f4069a4e18897
D5319 <ksh32@gsretail.com>
2024-11-25 11:02:57 +0900

    241125 Initialize

---
+++ src/test/java/wa/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package wa.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
